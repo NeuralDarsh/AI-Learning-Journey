39c6f668637824f87354d2b825ee3638cb5219f6
codedarsh <darshanbagale2@gmail.com>
2026-01-28 20:13:01 +0530

    Integrate live weather API using requests library
    Implemented a Python script to fetch real-time data from the Tokyo weather server. Practiced error handling with try-except blocks and external package management.

---
+++ tokyo_weather.py
@@ -0,0 +1,28 @@
+import requests 
+ # This library lets your code talk to the internet
+
+def get_tokyo_weather():
+    # We are using a free weather service
+    city = "Tokyo"
+    url = f"https://wttr.in/{city}?format=3"
+    
+    print(f"Connecting to Tokyo Weather Server...")
+    
+    try:
+        # This line sends a 'Request' to the server in Japan
+        response = requests.get(url)
+        
+        # 200 means 'Success' in the world of internet networking
+        if response.status_code == 200:
+            print("\n--- LIVE TOKYO UPDATE ---")
+            print(f"Current Status: {response.text}")
+            print(f"Goal: Live A Simple And Quiet Life In Japan ")
+        else:
+            print("Server is busy. Try again in a few minutes.")
+            
+    except Exception as e:
+        # This handles errors if your internet is disconnected
+        print(f"Connection Error: {e}")
+
+# This line actually 'calls' the function to start the program
+get_tokyo_weather()
